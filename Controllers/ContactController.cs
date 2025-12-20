621fe3b8df34f17aa9d1703094a30bf204337c03
teymuri-alireza <teymuri.alireza98@gmail.com>
2025-12-20 13:01:34 +0330

    feat: add contact information

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            var info = new ContactInfo()
+            {
+                Email = "teymuri.alireza98@gmail.com",
+                Telegram = "@teymuri_alireza",
+                Location = "ایران، تهران"
+            };
+            return View(info);
+        }
+    }
+}
---
+++ Models/ContactInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolio.Models
+{
+    public class ContactInfo
+    {
+        public int Id { get; set; }
+        public required string Email { get; set; }
+        public required string Telegram { get; set; }
+        public required string Location { get; set; }
+    }
+}
