dc96c7b62932796533caf52d1ce97eedf7e2d1e8
teymuri-alireza <teymuri.alireza98@gmail.com>
2025-12-20 12:28:08 +0330

    faet(cursor): add glow cursor

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const glowCursor = document.getElementById("glow-cursor");
+
+document.addEventListener("mousemove", (e) => {
+    glowCursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
