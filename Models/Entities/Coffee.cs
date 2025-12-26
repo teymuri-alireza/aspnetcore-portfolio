a3c1a818b794809850039de88e76ee1f0565fd83
teymuri-alireza <teymuri.alireza98@gmail.com>
2025-12-26 22:38:48 +0330

    feat: add product model for coffee shop project

---
+++ Models/Entities/Coffee.cs
@@ -0,0 +1,16 @@
+﻿namespace MyPortfolio.Models.Entities
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int Price { get; set; }
+        public required bool Available { get; set; }
+        public required Category Type { get; set; }
+    }
+    public enum Category
+    {
+        Hot,
+        Iced
+    }
+}
