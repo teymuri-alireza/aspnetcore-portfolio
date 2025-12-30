52bfdb882c984356cba17539f60be5ae9d40dce3
teymuri-alireza <teymuri.alireza98@gmail.com>
2025-12-30 18:38:37 +0330

    feat(DB): connect to database and migrate

---
+++ Migrations/20251230145556_addImagePath.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class addImagePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "CoffeeProducts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "CoffeeProducts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CoffeeProducts",
+                table: "CoffeeProducts",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CoffeeProducts",
+                table: "CoffeeProducts");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "CoffeeProducts");
+
+            migrationBuilder.RenameTable(
+                name: "CoffeeProducts",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+        }
+    }
+}
