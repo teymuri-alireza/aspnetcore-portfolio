ed8c2ed9fb7f57dcd9d1170b8ed85d30fb885656
teymuri-alireza <teymuri.alireza98@gmail.com>
2025-12-31 12:43:46 +0330

    feat: redirect contact page to coffee shop project page

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -7,13 +7,14 @@ namespace MyPortfolio.Controllers
     {
         public IActionResult Index()
         {
-            var info = new ContactInfo()
-            {
-                Email = "teymuri.alireza98@gmail.com",
-                Telegram = "@teymuri_alireza",
-                Location = "ایران، تهران"
-            };
-            return View(info);
+            //var info = new ContactInfo()
+            //{
+            //    Email = "teymuri.alireza98@gmail.com",
+            //    Telegram = "@teymuri_alireza",
+            //    Location = "ایران، تهران"
+            //};
+            //return View(info);
+            return RedirectToAction("Index", "CoffeeShop");
         }
     }
 }
